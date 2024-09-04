9fcf16aa7b99296d288b89e2de57cc24c0cf2fa0
PRAVEEN KUMAR <102923982+pra-vee-n@users.noreply.github.com>
2024-09-04 16:48:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Zoopolis',
             appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-            timing: 20000, // 20 seconds
-            attempts: 20,
+            timing: 21000, // 20 seconds
+            attempts: 22,
         },
         2: {
             name: 'Chain Cube 2048',
@@ -37,8 +37,8 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Fluff Crusade',
             appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
-            timing: 20000, // 40 seconds
-            attempts: 30,
+            timing: 23000, // 40 seconds
+            attempts: 18,
         },
         4: {
             name: 'Train Miner',
@@ -72,8 +72,8 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Tile Trio',
             appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
-            timing: 20000, // 20 seconds
-            attempts: 20,
+            timing: 40000, // 20 seconds
+            attempts: 22,
         },
         9: {
             name: 'Mow and Trim',
